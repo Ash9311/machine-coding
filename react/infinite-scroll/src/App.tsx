3d459ca40d43547e105b2abad6712db35b247c87
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-05-01 23:27:34 +0530

    infinite scroll implementation

--- react/infinite-scroll/src/App.tsx
+++ react/infinite-scroll/src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState(50)
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.innerHeight + window.scrollY >= window.document.body.offsetHeight) {
+        setCount(count + 50);
+      }
+    }
 
+    window.addEventListener('scroll', onScroll);
+// memory cleanup, remove listener
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [count]);
+
+  let elements = [];
+
+  for (let i = 0; i < count; i++) {
+    elements.push(<div key={i}>{i}</div>)
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {elements}
     </>
   )
 }
