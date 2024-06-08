f8e1b7ee7abfb52ccdb98c77a4e540ff78cae785
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-06-09 00:45:24 +0530

    initial data

--- react/nested-comments/src/assets/data.ts
+++ react/nested-comments/src/assets/data.ts
@@ -2,27 +2,15 @@ export const commentsData = [
     {
         id: 1,
         text: "This is the first comment",
-        author: "User1",
+        author: "ash",
         replies: [
-            {
-                id: 2,
-                text: "This is a reply to the first comment",
-                author: "User2",
-                replies: [
-                    {
-                        id: 3,
-                        text: "This is a nested reply",
-                        author: "User3",
-                        replies: []
-                    }
-                ]
-            }
+
         ]
     },
     {
-        id: 4,
-        text: "This is another top-level comment",
-        author: "User4",
+        id: 2,
+        text: "This is second",
+        author: "aman",
         replies: []
     }
 ];
