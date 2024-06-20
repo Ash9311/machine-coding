060c2472b062f4966f23449dd6e55e84a7ef6399
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-06-20 23:57:33 +0530

    models

--- .net-core/TaskManagement/TaskManagement/Models/User.cs
+++ .net-core/TaskManagement/TaskManagement/Models/User.cs
@@ -2,5 +2,14 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Project> Project { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+        public ICollection<Comment> Comments { get; set; }
     }
 }
