274e8e2598456e54a2dd1d2af55ac888f49adb6a
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-06-26 22:29:21 +0530

    task management context

--- .net-core/TaskManagement/TaskManagement/Data/TaskManagementContext.cs
+++ .net-core/TaskManagement/TaskManagement/Data/TaskManagementContext.cs
@@ -1,6 +1,35 @@
-﻿namespace TaskManagement.Data
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Models;
+
+namespace TaskManagement.Data
 {
-    public class TaskManagementContext
+    public class TaskManagementContext:DbContext
     {
+        public TaskManagementContext(DbContextOptions<TaskManagementContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User>Users { get; set; }
+        public DbSet<Project>Projects { get; set; }
+        public DbSet<task>Tasks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.UserName)
+                .IsUnique();
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<Task>()
+                .Property(t => t.Status)
+                .HasConversion<string>();
+        }
+
     }
 }
